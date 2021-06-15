4d87b0c5c968815b8d90e4c9e489830074c38e4d
qinjiabo1990 <qinjiabo1990@gmail.com>
2021-06-15 20:13:02 +1000

    update sidebar

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,7 +1,15 @@
-function sidebar_open(){
-    document.getElementById('nav').style.display='block';
+function sidebar_open() {
+    const nav = document.getElementById('nav');
+    const toggle = document.getElementById('toggle');
+    nav.className = 'nav-open';
+    toggle.setAttribute('onclick', 'sidebar_close()')
+    toggle.innerHTML = '<i class="material-icons">close</i>'
 }
 
-function sidebar_close(){
-    document.getElementById('nav').style.display=`none`;
+function sidebar_close() {
+    const nav = document.getElementById('nav');
+    const toggle = document.getElementById('toggle');
+    nav.className = 'nav-close';
+    toggle.setAttribute('onclick', 'sidebar_open()')
+    toggle.innerHTML = '☰'
 }
